a7112189f624526d9f15ec1eb6d6fe9b586ea918
RIckwijnhold <rick.wijnhold@student.rocva.nl>
2019-03-05 11:44:42 +0100

    Hoofdstuk 11.5

---
+++ src/h011/Hoofdstuk115.java
@@ -0,0 +1,27 @@
+package h011;
+
+
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class Hoofdstuk115 extends Applet {
+    int x;
+    int y;
+
+
+    public void init() {
+    x = 10;
+    y =20;
+    }
+
+
+    public void paint(Graphics g) {
+        for (int i = 1; i < 1000 ; i++) {
+            g.drawRect(x,y,10,10);
+            x = x+10;
+            y= y+10;
+        }
+    }
+}
